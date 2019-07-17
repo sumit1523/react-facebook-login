0ca4e22c2db15cc71734fefee68bb2248fe584d2
sumit kumar <sumit1523@gmail.com>
2019-07-17 22:53:30 +0530

    facebook login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FacebookLoginWithButton from 'react-facebook-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const componentClicked = () => {
+  console.log( "Facebook Button Clicked!" )
 }
 
-export default App;
+const LoginButton = ({facebookResponse}) => (
+  <FacebookLoginWithButton
+    appId="1186061154906638"
+    // autoLoad
+    fields="name,email,picture"
+    onClick={componentClicked}
+    callback={facebookResponse}
+    icon="fa-facebook"/>
+  )
+
+
+const UserScreen = ({user}) => (
+  <>
+    <h1>Welcome {user.name}!</h1>
+    <p>{ user.email }</p>
+    <img src={user.picture.data.url} height={user.picture.height} width={user.picture.width} alt="avatar"/>
+  </>
+)
+
+class App extends React.Component {
+  state = {user:false}
+  facebookResponse = (response) => { console.log( response ); this.setState( {...this.state, user: response } ) }
+
+  render() {
+    return (
+      <div style={{ margin: "auto", textAlign: "center", paddingTop: "2em" }}>
+        { this.state.user ? <UserScreen user={this.state.user}/> :
+          <LoginButton facebookResponse={this.facebookResponse}/>
+        }
+      </div>
+    )
+  }
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+
+// import FacebookLogin from 'react-facebook-login';
+
+// import GoogleLogin from 'react-google-login';
+
+// class App extends Component {
+
+//   render() {
+
+//     const responseFacebook = (response) => {
+//       console.log(response);
+//     }
+
+//     const responseGoogle = (response) => {
+//       console.log(response);
+//     }
+
+//     return (
+//       <div className="App">
+//         <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+//         <FacebookLogin
+//           appId="1186061154906638" //APP ID NOT CREATED YET
+//           fields="name,email,picture"
+//           callback={responseFacebook}
+//         />
+//         <br />
+//         <br />
+
+
+//         <GoogleLogin
+//           clientId="507773353909-vg3h3s31ifehob4300kgf63qa18cij6s.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+//           buttonText="LOGIN WITH GOOGLE"
+//           onSuccess={responseGoogle}
+//           onFailure={responseGoogle}
+//         />
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
